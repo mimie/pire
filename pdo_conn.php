40c0516a64a4ef7fa59f8e1e8626de434a18ba7c
mimie <karen.convicto@yahoo.com>
2013-12-20 17:01:17 +0800

    change database connection

--- pdo_conn.php
+++ pdo_conn.php
@@ -2,7 +2,7 @@
 
   function civicrmConnect(){
 
-    $dbh = new PDO('mysql:host=10.110.215.92;dbname=iiap_civicrm_dev', 'iiap', 'mysqladmin');
+    $dbh = new PDO('mysql:host=10.110.215.92;dbname=webapp_civicrm', 'iiap', 'mysqladmin');
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $dbh;
 
