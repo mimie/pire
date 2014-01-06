3ce83adddcf6de7034fa8fcbd35002dd9e58adaf
sydricksamonte <karen.convicto@yahoo.com>
2014-01-06 11:27:36 +0800

    adjust html template for member billing reference

--- memberBillingReference.php
+++ memberBillingReference.php
@@ -1,15 +1,19 @@
+<?php
+/**   ob_start();
+   require dirname(__FILE__) . '/../../dompdf-master/dompdf_config.inc.php';**/
+?>
 <html>
 <head>
 <title>Membership Billing</title>
 <style>
 #main{
+  width: 699.21px;
   border: 3px solid #0000FF;
-  width: 739.28px;
-  height: 959.62px;
   background-image:url('images/watermark.png');
   background-repeat:no-repeat;
   background-position:center;
   background-size:350px 300px;
+  padding: 2px 2px 2px 2px;
 
 }
 
@@ -49,28 +53,28 @@
 <body>
 <?php
 
-  include 'pdo_conn.php';
   include 'weberp_functions.php';
+  include 'pdo_conn.php';
   include 'billing_functions.php';
   include 'send_functions.php';
   include 'membership_functions.php';
   include 'login_functions.php';
- 
+
   $dbh = civicrmConnect();
-  
+ 
   /**session_start();
   //if the user has not logged in
   if(!isLoggedIn())
   {
     header('Location: login.php');
     die();
-  }**/
+  }
+
+  $userId = $_GET["user"];
+  $generator = getUserFullName($dbh,$userId);**/
 
   @$billingId = $_GET["billingId"];
   $billingDetails = getMemberBillingDetails($dbh,$billingId);
-
-  //$userId = $_GET["user"];
-  //$generator = getUserFullName($dbh,$userId);
 
   $memberYear = $billingDetails["year"];
   $memberName = $billingDetails["member_name"];
@@ -90,33 +94,35 @@
 
 ?>
 <div id="main">
-  <div style="width:896.5px;height:7.93px;"></div>
-   <center>
-    <div id="header">
+  <!--<div style="width:896.5px;height:7.93px;"></div>-->
+  <div></div>
+<!--   <center>-->
+    <!--<div id="header">-->
      <table id="header">
        <tr>
-        <td rowspan="2"><img id="logo" src="iiap_logo.png"></td>
+        <td rowspan="2"><img id="logo" src="images/iiap_logo.png"></td>
         <td>Institute of Internal Auditors Philippines, Inc.</td>
        </tr>
       <tr>
         <td><font size="2">Unit 702 Corporate Center, 139 Valero St., Salcedo Village, Makati City 1227</font></td>
       </tr>
     </table>
-   </div>
-  </center>
-  <center>
+   <!--</div>-->
+<!--  </center>
+-->
+<!--  <center>-->
     <div id="tin">
-      <table align="left">
+      <table>
        <tr>
-         <td width="50%"><font style="font-size:12px"><b>TIN No. 001-772-403-000 : (+632) 940-9551 /940-9554 : Fax (+632) 325-0414</b></font></td>
-         <td width="50%" align="right" rowspan="2"><font style="font-size:48px;color:#0B3861;"><i>BILLING</i></font></td>
+         <td><font style="font-size:12px"><b>TIN No. 001-772-403-000 : (+632) 940-9551 /940-9554 : Fax (+632) 325-0414</b></font></td>
+         <td align="right" rowspan="2"><font style="font-size:48px;color:#0B3861;"><i>BILLING</i></font></td>
        </tr>
        <tr><td><br></td></tr>
      </table>
     </div>
     <!--billed to-->
     <div id="billedTo">
-     <table align="left">
+     <table>
       <tr>
          <td width="101.67px"><font style="font-size:16px"><b>BILLED TO:</b></font></td>
          <td width="329.95px"><font style="font-size:13px"><b><?=$memberName?></b></font></td>
@@ -138,7 +144,7 @@
       <tr>
          <td></td>
          <td colspan="2" style="border-right:2px solid black;"><font style="font-size:13px"><?=$phone?></font></td>
-         <!--<td style="border-right:2px solid black;">Something</td>-->
+         <td></td>
          <td></td>
       </tr>
       <tr>
@@ -148,15 +154,16 @@
      </table>
      <!--end of billed to-->
     </div>
+    
     <div id="billedTo">
      <!--particulars-->
-     <table align="left" style="border-collapse:collapse;" border="1px">
+     <table style="border-collapse:collapse;" border="1px">
       <tr>
-        <td colspan="2" width="566.93px" align="center" bgcolor="#0B0B3B" style="border:2px solid #BCF5A9"><font style="font-size:13px;color:white;"><b>PARTICULARS</b></font></td>
-        <td width="132.28px" align="center" bgcolor="#0B0B3B" style="border:2px solid #BCF5A9"><font style="font-size:13px;color:white;"><b>AMOUNT</b></font></td>
+        <td colspan="2" align="center" bgcolor="#0B0B3B" style="border:2px solid #BCF5A9"><font style="font-size:13px;color:white;"><b>PARTICULARS</b></font></td>
+        <td align="center" bgcolor="#0B0B3B" style="border:2px solid #BCF5A9"><font style="font-size:13px;color:white;"><b>AMOUNT</b></font></td>
       </tr>   
       <tr>
-        <td colspan="2" height="350px" style="border:2px solid #BCF5A9; vertical-align:top;">
+        <td colspan="2" height="275px" style="border:2px solid #BCF5A9; vertical-align:top;">
           Institute of Internal Auditors Philippines, Inc.<br>
           Membership Annual Registration&nbsp;<?=$memberYear?><br> 
         </td>
@@ -171,7 +178,7 @@
      </div>
 
      <div id = "billedTo">
-     <table align="left">
+     <table>
       <tr>
        <td width="279.61px" style="vertical-align:top">
           <br><font style="font-size:13px;font-family:Arial"><b>DIRECT ALL INQUIRIES TO:</b></font><br>
@@ -188,13 +195,27 @@
             <font style="font-size:13px;font-family:Arial">&nbsp;&nbsp;&Oslash;&nbsp;If by SM Department Store Bills Payment Center,<br>
           Please indicate the SM Department Store branch where you are transacting and present your<br>
           <b>ORIGINAL COPY</b> of the receipt of the payment.
-          </font>
-          
+<!--            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;payment to cover for bank charges.</font><br><br>
+          <font style="font-size:13px;font-family:Arial">&nbsp;&nbsp;&Oslash;&nbsp;By direct deposit to:</font><br>
+          <font style="font-size:13px;font-family:Arial">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Institute of Intenal Auditors Philippines, Inc.</b></font><br>
+          <font style="font-size:13px;font-family:Arial">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Acct No. 691-002-1745</b></font><br>
+          <font style="font-size:13px;font-family:Arial">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Banco de Oro - Rufino Branch</b></font><br><br>
+         <font style="font-size:13px;font-family:Arial">
+           Please indicate the BDO branch where you are transacting and<br>
+           present your <b>ORIGINAL COPY</b> of the bank validated deposit slip<br>
+           to claim your OFFICIAL RECEIPT. 
+-->
+         </font>
        </td>
       </tr>
      </table>
     </div>
-  </center>
+<!--  </center>-->
 </div>
 </body>
 </html>
+<?php
+  /**$html = ob_get_clean();
+  $file = "membershipIndividualBilling.php";
+  generatePDFBilling($html,$billingNo,$file);**/
+?>
