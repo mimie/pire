74eb5bb4db9f2c29c90b757d65fd4784f09f5a5e
mimie <karen.convicto@yahoo.com>
2014-01-20 16:57:16 +0800

    added action process

--- notes/notes.php
+++ notes/notes.php
@@ -1,11 +1,45 @@
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>Notes Billing</title>
+  <link rel="stylesheet" type="text/css" href="../billingStyle.css">
+  <link rel="stylesheet" type="text/css" href="../menu.css">
+  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+  <script src="../js/jquery-jPaginate.js"></script>
+  <script src="../js/jquery.tablesorter.js"></script>
+</head>
+<body>
 <?php
 
   include '../pdo_conn.php';
+  include '../login_functions.php';
   include 'notes_functions.php';
 
   $dbh = civicrmConnect();
   $notes = getAllNotes($dbh);
+  $logout = logoutDiv($dbh);
+  echo $logout;
+  echo "<br>";
 
+?>
+
+<form method="POST" action="">
+  <div align="center">
+    Select action process:<br>
+    <select name="actions">
+     <option value="select">- Select action type-</option>
+     <option value="" disabled></option>
+     <option value="edit">Edit</option>
+     <option value="delete">Delete</option>
+    </select>
+    <input type="submit" value="Process Action">
+  </div>
+
+<?php
+
+  echo "<br>";
+  echo "<div align='center'>";
   echo "<table>"
        ."<thead>"
        ."<tr>"
@@ -34,6 +68,9 @@
   }
 
   echo "</tbody></table>";
-  
+  echo "</div>";
   
 ?>
+</form>
+</body>
+</html>
