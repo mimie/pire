258c2a3dedf570bfedf787de4c00cc80af65c864
mimie <karen.convicto@yahoo.com>
2014-01-20 16:57:59 +0800

    added function getNoteById

--- notes/notes_functions.php
+++ notes/notes_functions.php
@@ -11,6 +11,21 @@ function getAllNotes($dbh){
   return $result;
 }
 
+function getNoteById($dbh,$noteId){
+
+  
+  $sql = $dbh->prepare("SELECT notes_id, notes.notes_category_id,category.category_name,notes,notes_status
+                        FROM billing_notes notes, billing_notes_category category
+                        WHERE notes.notes_category_id = category.notes_category_id
+                        AND notes_id = ?
+                       ");
+  $sql->bindValue(1,$noteId,PDO::PARAM_INT);
+  $sql->execute();
+  $result = $sql->fetch(PDO::FETCH_ASSOC);
+  return $result;
+ 
+}
+
 
  
 ?>
