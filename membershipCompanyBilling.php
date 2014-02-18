6ba5484ce368bbfdcde3f0e64cd4f0c52d5b629a
mimie <karen.convicto@yahoo.com>
2014-02-18 11:02:40 +0800

    change the link for individual membership billing

--- membershipCompanyBilling.php
+++ membershipCompanyBilling.php
@@ -49,7 +49,7 @@ $(function() {
    <table width='100%'>
     <tr>
      <td align='center' bgcolor='#084B8A'><a href='membershipNewBilling.php'>NEW MEMBERSHIP BILLING</a></td>
-     <td align='center' bgcolor="#084B8A"><a href='membershipIndividualBilling.php'>INDIVIDUAL BILLING</a></td>
+     <td align='center' bgcolor="#084B8A"><a href='membershipIndividualBilling2.php'>INDIVIDUAL BILLING</a></td>
      <td align='center' bgcolor='white'><a href='membershipCompanyBilling.php'>COMPANY BILLING</td>
      <td align='center' bgcolor='#084B8A'><a href='onlineMembership.php'>ONLINE MEMBERSHIP</td>
      <td align='center' bgcolor='#084B8A'><a href='membershipBillingView.php'>GENERATED BILLINGS</td>
