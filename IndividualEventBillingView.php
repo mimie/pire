8f32ece467183ff111510f8c91af39c52f666d54
mimie <karen.convicto@yahoo.com>
2014-02-20 15:11:33 +0800

    added search options for billing

--- IndividualEventBillingView.php
+++ IndividualEventBillingView.php
@@ -32,12 +32,30 @@ $(function() {
   $dbh = civicrmConnect();
   $menu = logoutDiv($dbh);
   echo $menu;
+  echo "<br>";
+
+  echo "<div style='width:80%;margin:0 auto;padding:3px;'>";
+  echo "<form action='' method='POST'>"
+       . "<fieldset>"
+       . "<legend>Search Individual Event Billing</legend>"
+       . "Search category:&nbsp;"
+       . "<select name='searchType'>"
+       . "<option value='name'>Name</option>"
+       . "<option value='orgname'>Organization Name</option>"
+       . "<option value='eventname'>Event Name</option>"
+       . "<option value='billingno'>Billing No.</option>"
+       . "<option value='eventtype'>Event Type</option>"
+       . "</select>"
+       . "&nbsp;<input type='text' name='searchText' placeholder='Enter search text here...' size='30'>"
+       . "<input type='submit' name='search' value='SEARCH'>"
+       . "</fieldset><br>";
+
 
   $billings = getAllIndividualBillings($dbh);
-  echo "<pre>";
-  print_r($billings);
-  echo "</pre>";
-  
+  $display = displayIndividualEventBillings($billings);
+  echo $display;  
 ?>
+  </div>
+  </form>
 </body>
 </html>
--- billingview_functions.php
+++ billingview_functions.php
@@ -3,7 +3,7 @@
 function getAllIndividualBillings($dbh){
 
   $sql = $dbh->prepare("SELECT bd.participant_id,bd.contact_id,
-                        cc.sort_name,event_type,event_name,
+                        cc.sort_name,event_type,event_name,cp.event_id,
                         cc.organization_name,cps.name as status,
                         bd.fee_amount,bd.billing_no,bill_date,
                         cp.fee_amount as current_amount
@@ -16,5 +16,58 @@ function getAllIndividualBillings($dbh){
  $result = $sql->fetchAll(PDO::FETCH_ASSOC);
 
  return $result;
+}
+
+function displayIndividualEventBillings(array $billings){
+
+  $html = "<table id='billings' width='100%'>"
+        . "<th>Participant Id</th>"
+        . "<th>Participant Name</th>"
+        . "<th>Event Type</th>"
+        . "<th>Event Name</th>"
+        . "<th>Organization Name</th>"
+        . "<th>Participant Status</th>"
+        . "<th>Fee Amount</th>"
+        . "<th>Billing Number</th>"
+        . "<th>Billing Date</th>"
+        . "<th>Print Bill</th>"
+        . "<th>Amount Change</th>";
+  $html = $html."<tbody>";
+
+  foreach($billings as $key => $field){
+    $participantId = $field["participant_id"];
+    $name = $field["sort_name"];
+    $name = mb_convert_encoding($name,"UTF-8");
+    $eventType = $field["event_type"];
+    $eventName = $field["event_name"];
+    $orgName = $field["organization_name"];
+    $orgName = mb_convert_encoding($orgName,"UTF-8");
+    $status = $field["status"];
+    $amount = $field["fee_amount"];
+    $billingNo = $field["billing_no"];
+    $billingDate = $field["bill_date"];
+    $amountChange = $field["current_amount"];
+    $amountChange = $amount == $amountChange ? '' : $amountChange;
+    $eventId = $field["event_id"];
+
+    $html = $html."<tr>"
+          . "<td>$participantId</td>"
+          . "<td>$name</td>"
+          . "<td>$eventType</td>"
+          . "<td>$eventName</td>"
+          . "<td>$orgName</td>"
+          . "<td>$status</td>"
+          . "<td>$amount</td>"
+          . "<td>$billingNo</td>"
+          . "<td>$billingDate</td>"
+          . "<td><a href='individualBillingReference.php?billingRef=$billingNo&eventId=$eventId' target='_blank'><img src='images/printer-icon.png' height='40' width='40'></a></td>"
+          . "<td>$amountChange</td>"
+          . "</tr>";
+
+  }
+  $html = $html."</tbody></table>";
+  
+  return $html;
+
 }
 ?>
