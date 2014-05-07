49552ff9d229f035160ea4af10aeae53733fa115
mimie <karen.convicto@yahoo.com>
2014-05-07 10:37:39 +0800

    added page for editing package name

---
+++ packages/EditPackage.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+ <title>
+ </title>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="billingStyle.css">
+<link rel="stylesheet" type="text/css" href="menu.css">
+<style type="text/css">
+   body{overflow:scroll;}
+</style>
+
+</head>
+<body>
+<?php
+?>
+</body>
+</html>
