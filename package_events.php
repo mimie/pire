8f9222628b74366e9bd5a3e3ff3d26cca439797a
mimie <karen@imperium.ph>
2014-05-29 17:53:40 +0800

    added page for generating package events

---
+++ package_events.php
@@ -0,0 +1,65 @@
+<html>
+<head>
+<title>Generate Package Events Bill</title>
+<link rel="stylesheet" type="text/css" href="billingStyle.css">
+<link rel="stylesheet" type="text/css" href="menu.css">
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+  <script src="js/jquery-jPaginate.js"></script>
+  <script src="js/jquery.tablesorter.js"></script>
+<script type='text/javascript' language='javascript'>
+function reloadPage()
+  {
+  location.reload();
+  }
+$(function() {
+        $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+        $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+        $('#packages').jPaginate({
+                'max': 15,
+                'page': 1,
+                'links': 'buttons'
+        });
+//        $("table").tablesorter( {sortList: [[0,0], [1,0]]} ); 
+});
+$(function() {
+    $( "#confirmation" ).dialog({
+      resizable: false,
+      width:500,
+      modal: true,
+      buttons: {
+        "OK": function() {
+          //$( this ).dialog( "close" );
+          reloadPage();
+        }
+      }
+    });
+});
+</script>
+</head>
+<body>
+<?php
+
+  include 'pdo_conn.php';
+  include 'shared_functions.php';
+  include 'login_functions.php';
+  include 'bir_functions.php';
+  include 'packages/packagebill_functions.php';
+
+  $dbh = civicrmConnect();
+  $menu = logoutDiv($dbh);
+  echo $menu;
+  echo "<br>";
+  $uid = $_GET['uid'];
+  $pid = $_GET['pid'];
+
+  $participants = getParticipantsPerPackage($pid);
+  echo "<pre>";
+  print_r($participants);
+  echo "</pre>";
+
+
+?>
+</body>
+</html>
