ea4424474fc91dc52f0af4733e2077c41f1c6190
mimie <karen@imperium.ph>
2014-06-23 15:46:20 +0800

    display billing details for the edited amount of individual bill

--- edit_individual.php
+++ edit_individual.php
@@ -7,10 +7,43 @@
        
         $billing_no = $_GET['billing_no'];
         $bill = getInfoByBillingNo($billing_no);
-        echo "<pre>";
-        print_r($bill);
-        echo "</pre>";
+        $address = $bill['street_address']." ".$bill['city_address'];
 
 ?>
+	<table>
+		<tr>
+			<th colspan='2'>Billing Information</th>
+		</tr>
+		<tr>
+			<th>Billing No.</th><td><?=$billing_no?></td>
+		</tr>
+		<tr>
+			<th>Name</th><td><?=$bill['sort_name']?></td>
+		</tr>
+		<tr>
+			<th>Event Name</th><td><?=$bill['event_name']?></td>
+		</tr>
+		<tr>
+			<th>Event Type</th><td><?=$bill['event_type']?></td>
+		</tr>
+		<tr>
+			<th>Email</th><td><?=$bill['email']?></td>
+		</tr>
+		<tr>
+			<th>Organization</th><td><?=$bill['organization_name']?></td>
+		</tr>
+		<tr>
+			<th>Address</th><td><?=$address?></td>
+		</tr>
+		<tr>
+			<th>Current Amount</th><td><?=$bill['current_amount']?></td>
+		</tr>
+		<tr>
+			<th>Civicrm Amount</th><td><?=$bill['civicrm_amount']?></td>
+		</tr>
+		<tr>
+			<th>Participant Status</th><td><?=$bill['status']?></td>
+		</tr>
+	</table>
 </body>
 </html>
