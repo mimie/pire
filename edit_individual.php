36c9e76bfc1ffd4a072d0ac194be27ae120cbbb8
mimie <karen@imperium.ph>
2014-06-23 14:14:37 +0800

    get bill information of the participant

--- edit_individual.php
+++ edit_individual.php
@@ -2,6 +2,14 @@
 <head><title>Edit Individual Bill</title></head>
 <body>
 <?php
+	include 'pdo_conn.php';
+        include 'bir_functions.php';
+       
+        $billing_no = $_GET['billing_no'];
+        $bill = getInfoByBillingNo($billing_no);
+        echo "<pre>";
+        print_r($bill);
+        echo "</pre>";
 
 ?>
 </body>
