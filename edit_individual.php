0013ef089b8cffb1cea677d10c5f198b6d480d1e
mimie <karen@imperium.ph>
2014-06-23 19:26:29 +0800

    added conditions in updating billing information

--- edit_individual.php
+++ edit_individual.php
@@ -7,10 +7,17 @@
 <?php
 	include 'pdo_conn.php';
         include 'bir_functions.php';
+        include 'editbill_functions.php';
        
         $billing_no = $_GET['billing_no'];
         $bill = getInfoByBillingNo($billing_no);
         $address = $bill['street_address']." ".$bill['city_address'];
+        $status = $bill['participant_status'];
+        $isEdit = $bill['edit_bill'];
+        $current_amount = sprintf('%0.2f', $bill['current_amount']);
+        $civicrm_amount = $bill['civicrm_amount'];
+        $eventId = $bill['event_id'];
+        $allowedEdit_info = $isEdit == '0' || ($isEdit == 1 && $civicrm_amount == $current_amount) ? 'Billing information cannot be updated.' : 'Update Billing Information';
 ?>
       <div align='center'>
 	<table>
@@ -18,7 +25,10 @@
 			<th colspan='2'>Billing Information</th>
 		</tr>
 		<tr>
-			<th>Billing No.</th><td><?=$billing_no?></td>
+			<th>Reference No.</th><td><?=$billing_no?></td>
+		</tr>
+		<tr>
+			<th>BS No.</th><td><?=$bill['bir_no']?></td>
 		</tr>
 		<tr>
 			<th>Name</th><td><?=$bill['sort_name']?></td>
@@ -39,14 +49,44 @@
 			<th>Address</th><td><?=$address?></td>
 		</tr>
 		<tr>
-			<th>Current Amount</th><td><?=$bill['current_amount']?></td>
+			<th>Current Amount</th><td><?=$current_amount?></td>
 		</tr>
 		<tr>
-			<th>Civicrm Amount</th><td><?=$bill['civicrm_amount']?></td>
+			<th>Civicrm Amount</th><td><?=$civicrm_amount?></td>
 		</tr>
 		<tr>
-			<th>Participant Status</th><td><?=$bill['participant_status']?></td>
+			<th>Participant Status</th><td><?=$status?></td>
 		</tr>
+                <tr>
+                	<th colspan='2'><?=$allowedEdit_info?></th>
+                </tr>
+<form action='' method='POST'>
+<?php
+	if($status == 'Cancelled' && $isEdit == 1){
+                echo "<tr>";
+	        echo "<td>Change Name</td><td><SELECT>";
+		$participants = getParticipantWithSameAmount($eventId,$civicrm_amount);
+                foreach($participants as $key=>$field){
+                        $participant_id = $field['participant_id'];
+                        $name = $field['sort_name'];
+                        $amount = $field['fee_amount'];
+			echo "<option value='".$participant_id."'>".$name."-".$amount."</option>";
+                }
+		echo "</SELECT><input type='submit' name='update' value='UPDATE BILL'></td></tr>";
+
+        }elseif($status !='Cancelled' && $current_amount!=$civicrm_amount && $isEdit == 1){
+               echo "<tr>";
+               echo "<td>Change Amount</td><td><input type='text' name='new_amount' value='$civicrm_amount' readonly>";
+               echo "<input type='submit' name='update' value='UPDATE BILL'></td></tr>";
+           
+        }elseif($status !='Cancelled' && $current_amount!=$civicrm_amount && $isEdit == 0){
+	       echo "<tr>";
+               echo "<td>Generate Bill</td><td><input type='text' name='bs_no' placeholder='000001' required/>";
+               echo "<input type='submit' value='generate' value='GENERATE BILL'></td></tr>";
+         }
+
+?>
+</form>
 	</table>
       </div>
 </body>
