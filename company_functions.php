0de326218fbb590d73b13a32295ff0df507bf661
mimie <karen@imperium.ph>
2014-06-27 15:52:55 +0800

    make the participant id the key in fetching newly added billings

--- company_functions.php
+++ company_functions.php
@@ -3,30 +3,35 @@
 
 function getNewlyAddedBillings($dbh,$eventId,$orgId){
 
-  $sql = $dbh->prepare("SELECT cp.id as participant_id, cc.display_name as name,cc.id as contact_id, cp.fee_amount, cps.label as status
-                        FROM civicrm_participant cp, civicrm_value_billing_17 billing, civicrm_contact cc, civicrm_participant_status_type cps
-                        WHERE cp.id = billing.entity_id
-                        AND billing.billing_45 = 'Company'
-                        AND cp.event_id = ?
-                        AND cc.employer_id = ?
-                        AND cc.is_deleted = 0
-                        AND cp.contact_id = cc.id
-                        AND cp.status_id NOT IN (4,7,15,17)
-                        AND cp.status_id = cps.id
-                        AND cp.id NOT IN (SELECT bd.participant_id FROM billing_details bd
-                                          WHERE bd.event_id = ?
-                                          AND bd.org_contact_id = ?
-                                          AND billing_type = 'Company')
-                       ");
-  $sql->bindValue(1,$eventId,PDO::PARAM_INT);
-  $sql->bindValue(2,$orgId,PDO::PARAM_INT);
-  $sql->bindValue(3,$eventId,PDO::PARAM_INT);
-  $sql->bindValue(4,$orgId,PDO::PARAM_INT);
-  $sql->execute();
- 
-  $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+	try{
 
-  return $result;
+		  $sql = $dbh->prepare("SELECT cp.id as participant_id, cc.display_name as name,cc.id as contact_id, cp.fee_amount, cps.label as status
+					FROM civicrm_participant cp, civicrm_value_billing_17 billing, civicrm_contact cc, civicrm_participant_status_type cps
+					WHERE cp.id = billing.entity_id
+					AND billing.billing_45 = 'Company'
+					AND cp.event_id = ?
+					AND cc.employer_id = ?
+					AND cc.is_deleted = 0
+					AND cp.contact_id = cc.id
+					AND cp.status_id NOT IN (4,7,15,17)
+					AND cp.status_id = cps.id
+					AND cp.id NOT IN (SELECT bd.participant_id FROM billing_details bd
+							  WHERE bd.event_id = ?
+							  AND bd.org_contact_id = ?
+							  AND billing_type = 'Company')
+				       ");
+		  $sql->bindValue(1,$eventId,PDO::PARAM_INT);
+		  $sql->bindValue(2,$orgId,PDO::PARAM_INT);
+		  $sql->bindValue(3,$eventId,PDO::PARAM_INT);
+		  $sql->bindValue(4,$orgId,PDO::PARAM_INT);
+		  $sql->execute();
+		 
+		  $result = $sql->fetchAll(PDO::FETCH_GROUP|PDO::FETCH_UNIQUE);
+
+		  return $result;
+       }catch(PDOException $error){
+		echo $error->getMessage();
+	}
 
 }
 
