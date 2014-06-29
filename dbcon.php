165015b9882a39ebef32330ee55dd42aa574bd49
mimie <karen.convicto@yahoo.com>
2014-06-29 17:25:26 +0800

    added database connection

--- dbcon.php
+++ dbcon.php
@@ -1,10 +1,11 @@
 <?php
-  
-  $db=mysql_connect('10.110.215.92', 'iiap', 'mysqladmin');
+
+  date_default_timezone_set('Asia/Manila');
+
+  $db=mysql_connect('localhost', 'root', 'mysqladmin');
   if (!$db) {
       die('Could not connect: ' . mysql_error());
   }
 
   mysql_select_db("webapp_civicrm", $db);
 ?>
-
--- pdo_conn.php
+++ pdo_conn.php
@@ -1,8 +1,10 @@
-<?
+<?php
+
+  date_default_timezone_set('Asia/Manila');
 
   function civicrmConnect(){
 
-    $dbh = new PDO('mysql:host=10.110.215.92;dbname=webapp_civicrm', 'iiap', 'mysqladmin');
+    $dbh = new PDO('mysql:host=localhost;dbname=webapp_civicrm', 'root', 'mysqladmin');
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $dbh;
 
@@ -10,20 +12,20 @@
 
   function weberpConnect(){
 
-   $weberpConn = new PDO('mysql:host=10.110.215.92;dbname=IIAP_DEV','iiap','mysqladmin');
+   $weberpConn = new PDO('mysql:localhost;dbname=iiap_weberp2014','root','mysqladmin');
    $weberpConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
    return $weberpConn;
 
-  } 
+  }
 
   function civicrmDB($sql){
-    $dbh = new PDO('mysql:host=10.110.215.92;dbname=webapp_civicrm', 'iiap', 'mysqladmin');
+    $dbh = new PDO('mysql:host=localhost;dbname=webapp_civicrm', 'root', 'mysqladmin');
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = $dbh->prepare($sql);
-   
+
      return $sql;
 
 
-  } 
+  }
 
 ?>
