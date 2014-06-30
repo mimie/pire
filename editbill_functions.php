e70e71c7b6fd1a9af3bcff20c1a0759975507cb1
mimie <karen@imperium.ph>
2014-06-30 16:14:55 +0800

    added bir no parameter in getting billing information of a company

--- editbill_functions.php
+++ editbill_functions.php
@@ -133,19 +133,18 @@ function getCompanyNameByOrgId($orgId){
 	return $orgName;
 }
 
-function getCurrentCompanyBillByEvent($orgId,$eventId){
+function getCurrentCompanyBillByEvent($orgId,$eventId,$bir_no){
 
 	try{
-		$stmt = civicrmDB("SELECT max(cbid), bc.billing_no, bc.bir_no, bc.total_amount, bc.subtotal,bc.vat, bc.bill_date, bc.edit_bill,bc.notes_id,bc.nonvatable_type,bn.notes
+		$stmt = civicrmDB("SELECT cbid, bc.billing_no, bc.bir_no, bc.total_amount, bc.subtotal,bc.vat, bc.bill_date, bc.edit_bill,bc.notes_id,bc.nonvatable_type,bn.notes
 				   FROM billing_company bc 
                                    LEFT JOIN billing_notes bn ON bn.notes_id = bc.notes_id
 				   WHERE bc.event_id = ?
 				   AND bc.org_contact_id = ?
-				   AND is_cancelled = '0'
-				   AND is_void = '0'
-				   GROUP BY bc.org_contact_id, bc.event_id");
+				   AND bir_no = ?");
 		$stmt->bindValue(1,$eventId,PDO::PARAM_INT);
 		$stmt->bindValue(2,$orgId,PDO::PARAM_INT);
+                $stmt->bindValue(3,$bir_no,PDO::PARAM_STR);
 		$stmt->execute();
 		$result = $stmt->fetch(PDO::FETCH_ASSOC);
 
