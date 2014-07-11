ea40a7ff850ac19095d963506f31469ca33318a3
mimie <karen@imperium.ph>
2014-07-11 12:35:37 +0800

    fix tin

--- BIRForm/print_bir.php
+++ BIRForm/print_bir.php
@@ -175,7 +175,7 @@ p.issuedby{
 ?>
 <p class="myname"><?=$bill['sort_name']?></p>
 <p class="myaddress"><?=wordwrap($address,58,"<br>\n",TRUE)?></p>
-<p class="mytin">Tin</p>
+<p class="mytin"></p>
 <p class="lbltxn">Txn. No:</p>
 <p class="myrefno"><?=$reference_no = $bill['bir_no'] == NULL ? $billing_no : "BS-".$bill['bir_no']."/".$billing_no?></p>
 <p class="mybilldate"><?=date("F j, Y",strtotime($bill['bill_date']))?></p>
