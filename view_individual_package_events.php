5bb4d3ad9ad4820baf36771fc24040059916afbc
mimie <karen@imperium.ph>
2014-07-17 18:30:53 +0800

    fix print link

--- view_individual_package_events.php
+++ view_individual_package_events.php
@@ -157,14 +157,15 @@ function validator(){
    foreach($bills as $key=>$field){
 
          $bir_no = $field['bir_no'];
-	 $print_img = $bir_no == NULL || $field['edit_bill'] == 0 ? '' : "<img src='printer-icon.png' width='30' height='30'>";
+         $billing_no = $field['billing_no'];
+	 $print_img = $bir_no == NULL || $field['edit_bill'] == 0 ? '' : "<a href='BIRForm/print_package_individual.php?billing_no=".$billing_no."&uid=".$uid."' targer='_blank'><img src='printer-icon.png' width='30' height='30'></a>";
    	 $display = $display."<tr>"
                   . "<td>".$field['sort_name']."</td>"
                   . "<td>".$field['organization_name']."</td>"
                   . "<td>".number_format($field['total_amount'],2)."</td>"
                   . "<td>".number_format($field['subtotal'],2)."</td>"
                   . "<td>".number_format($field['vat'],2)."</td>"
-                  . "<td><a href='BIRForm/birform_package_individual.php?billing_no=".$field['billing_no']."&uid=".$uid."' target='_blank'>$preview_img</a>"
+                  . "<td><a href='BIRForm/birform_package_individual.php?billing_no=".$billing_no."&uid=".$uid."' target='_blank'>$preview_img</a>"
                   . "$print_img"
                   . "</td>"
                   . "<td>".number_format($field['amount_paid'],2)."</td>"
