5bdedd4a403d6d7713c686dba041cbdbb14e1ba2
mimie <karen@imperium.ph>
2014-07-21 17:40:29 +0800

    add css links

--- edit_individual_package.php
+++ edit_individual_package.php
@@ -1,6 +1,43 @@
 <html>
 <head>
 <title>Edit Individual Package</title>
+<link rel="stylesheet" type="text/css" href="billingStyle.css">
+<link rel="stylesheet" type="text/css" href="menu.css">
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+  <script src="js/jquery-jPaginate.js"></script>
+  <script src="js/jquery.tablesorter.js"></script>
+<script type='text/javascript' language='javascript'>
+function reloadPage()
+  {
+    //location.reload();
+    window.location=window.location;
+  }
+$(function() {
+        $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+        $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+        $('#billings').jPaginate({
+                'max': 10,
+                'page': 1,
+                'links': 'buttons'
+        });
+//        $("table").tablesorter( {sortList: [[0,0], [1,0]]} );
+});
+$(function() {
+    $( "#confirmation" ).dialog({
+      resizable: false,
+      width:500,
+      modal: true,
+      buttons: {
+        "OK": function() {
+          //$( this ).dialog( "close" );
+          reloadPage();
+        }
+      }
+    });
+});
+</script>
 </head>
 <body>
 <?php
