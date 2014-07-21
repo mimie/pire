df57a63cdfed8f963833f72089657a4ad5553d01
mimie <karen@imperium.ph>
2014-07-21 20:10:38 +0800

    display the list of events of the participant

--- edit_individual_package.php
+++ edit_individual_package.php
@@ -41,7 +41,118 @@ $(function() {
 </head>
 <body>
 <?php
+  include 'pdo_conn.php';
+  include 'login_functions.php';
+  include 'bir_functions.php';
+  include 'notes/notes_functions.php';
+  include 'packages/packagebill_functions.php';
+  include 'packages/package_functions.php';
+  include 'billing_functions.php';
+  include 'shared_functions.php';
+  include 'company_functions.php';
+
+  @$billing_no = $_GET['billing_no'];
+  @$bir_no = $_GET['bir_no'];
+  @$billing_id = $_GET['billing_id'];
+  @$uid = $_GET['uid'];
+  @$pid = $_GET['pid'];
+
+  $events = getEventsPerPackage($pid);
+  $package_name = getPackageName($pid);
 
 ?>
+	<div align='center'>
+		<table>
+			<tr><th colspan='4'><?=$package_name?></th></tr>
+                        <tr>
+			    <th>Event Id</th>
+			    <th>Event Name</th>
+			    <th>Start Date</th>
+			    <th>End Date</th>
+                        </tr>
+<?php
+	foreach($events as $key=>$field){
+?>
+        <tr>
+	    <td><?=$field['event_id']?></td>
+	    <td><?=$field['event_name']?></td>
+	    <td><?=date_standard($field['start_date'])?></td>
+	    <td><?=date_standard($field['end_date'])?></td>
+        </tr>
+
+<?php
+	}
+
+  echo "</table></br></br>";
+
+  $bill = getBillDetailsByBillingNo($billing_no);
+  $address = $bill['street_address']." ".$bill['city_address'];
+  $infobill = getEventBillDetailsByBillingNo($billing_no);
+?>
+    		
+	        <table align='center'>
+                	<tr>
+			    <th>Registration No.</th><td><?=$billing_no?></td>
+                        </tr>
+                        <tr>
+			    <th>ATP</th><td><?=$bir_no?></td>
+                        </tr>
+                        <tr>
+			    <th>Name</th><td><?=$bill['sort_name']?></td>
+                        </tr>
+                        <tr>
+			    <th>Package</th><td><?=$package_name?></td>
+                        </tr>
+                        <tr>
+			    <th>Address</th><td><?=$address?></td>
+                        </tr>
+                        <tr>
+			    <th>Current Amount</th><td><?=number_format($bill['total_amount'],2)?></td>
+                        </tr>
+                        <tr>
+			    <th>Subtotal</th><td><?=number_format($bill['subtotal'],2)?></td>
+                        </tr>
+                        <tr>
+			    <th>VAT</th><td><?=number_format($bill['vat'],2)?></td>
+                        </tr>
+                        <tr>
+			    <th>Notes</th><td><?=$bill['notes']?></td>
+                	</tr>
+                </table></br></br>
+                <table align='center'>
+                	<tr>
+				<th colspan='5' >LIST OF EVENTS</th>
+                	</tr>
+                        <tr>
+				<th>Participant Id</th>
+				<th>Event Name</th>
+				<th>Status</th>
+				<th>Current Amount</th>
+				<th>Civicrm Amount</th>
+                        </tr>
+<?php
+
+	foreach($infobill as $key=>$details){
+		foreach($details as $key=>$field){
+
+                     $fee_amount = $field['fee_amount'];
+                     $civicrm_amount = $field['civicrm_amount'];
+                     $status = $field['status'];
+                     $disabled = $fee_amount == $civicrm_amount ? 'disabled' : '';
+?>
+			<tr>
+				<td><input type='checkbox' value='<?=$field['participant_id']?>' name='participantIds[]' <?=$disabled?>><?=$field['participant_id']?></td>
+                                <td><?=$field['event_name']?></td>
+                                <td><?=$field['status']?></td>
+                                <td><?=number_format($fee_amount,2)?></td>
+                                <td><?=number_format($civicrm_amount,2)?></td>
+			</tr>
+<?php
+                }
+        }
+?>
+              </table> 
+                </table>
+	</div>
 </body>
 </html>
