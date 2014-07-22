13977661e6191d52192ab3272afd388a92e7275a
mimie <karen@imperium.ph>
2014-07-22 10:31:09 +0800

    fix print of package company bill

--- BIRForm/print_package_company.php
+++ BIRForm/print_package_company.php
@@ -154,8 +154,10 @@ p.issuedby{
   include '../bir_functions.php';
   include '../notes/notes_functions.php';
   include '../packages/packagebill_functions.php';
+  include '../packages/package_functions.php';
   include '../billing_functions.php';
   include '../shared_functions.php';
+  include '../company_functions.php';
 
   $dbh = civicrmConnect();
   @$eventId = $_GET["event_id"];
@@ -164,13 +166,19 @@ p.issuedby{
   $generator = getGeneratorName($uid);
   @$billing_no = $_GET["billing_no"];
   $bill = getBillDetailsByBillingNo($billing_no);
-  $address = $bill['street_address']." ".$bill['city_address'];
+
+  $orgId = $bill['contact_id'];
+  $address = getCompanyAddress($dbh,$orgId);
+  $complete_address = $address['street_address']." ".$address['city_address'];
+  $tin = getTin($orgId);
+  $package_name = getPackageName($bill['pid']);
 
   $nonvatable_type = $bill['nonvatable_type'];
   $bir_no = $bill['bir_no'];
   $ref_no = $bir_no == NULL ? $billing_no : "BS-".$bir_no."/".$billing_no;
   $infobill = getEventBillDetailsByBillingNo($billing_no);
-  $due_date = date_standard($infobill[0]['start_date']);
+  $firstkey = array_keys($infobill)[0];
+  $due_date = date_standard($infobill[$firstkey][0]['start_date']);
 
   //update edit bill
   $update_stmt = civicrmDB("UPDATE billing_details_package SET edit_bill= '0' WHERE billing_no=?");
@@ -179,35 +187,30 @@ p.issuedby{
 
 ?>
 <p class="myname"><?=$bill['sort_name']?></p>
-<p class="myaddress"><?=wrapAddress($address)?></font></p>
-<p class="mytin"></p>
+<p class="myaddress"><?=wrapAddress($complete_address)?></font></p>
+<p class="mytin"><?=$tin?></p>
 <p class="lbltxn">Txn. No:</p>
 <p class="myrefno"><?=$ref_no?></p>
 <p class="mybilldate"><?=date_standard($bill['bill_date'])?></p>
 <p class="myduedate"><?=$due_date?></p>
 <p class="myparticulars">
   <?php
+      echo $package_name."</br></br>";
+  ?>
+       &nbsp;&nbsp;&nbsp;Billing of the ff. participants:</br></br>
+<?php
       $amounts = array();
-      foreach($infobill as $key=>$field){
-           echo $field['event_name']."</br>";
-           if($field['end_date']){
-           	echo "On ".date_standard($field['start_date'])." to ".date_standard($field['end_date'])."</br>"; 
-           }else{
-		echo "On ".date_standard($field['start_date'])."</br>";
-	    }
+      foreach($infobill as $contact_id=>$details){
+              $fee_amount = 0;
+              $name = getContactName($contact_id);
+              echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+              echo $name."</br>";
+	      foreach($details as $key=>$field){
+              		$fee_amount = $fee_amount + $field["fee_amount"];
+	      }
 
-           $location = formatEventLocation(getEventLocation($dbh,$eventId));
-
-           $fee_amount = $nonvatable_type == NULL ? $field['fee_amount'] : round($field['fee_amount']/1.12,2);
-  
-           if($location){
-              echo "At ".$location."<br>";
-              $amounts[] = "</br></br>".number_format($fee_amount,2);
-           }else{
-              $amounts[] = "</br>".number_format($fee_amount,2);
-            }
-           echo "</br></br>";      
-      }
+              $amounts[] = $fee_amount;
+       }
 ?>
 </p>
 <p class="notes">
@@ -219,9 +222,11 @@ p.issuedby{
   ?>
 </p>
 <p class="myamount">
+  </br></br></br></br>
   <?php
       foreach($amounts as $display_amount){
-        echo $display_amount."</br></br></br>";
+        $display_amount = $nonvatable_type == NULL ? $display_amount : round(($display_amount/1.12),2);
+        echo number_format($display_amount,2)."</br>";
       }
   ?>
 </p>
